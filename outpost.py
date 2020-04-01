b9bf38d19348cd94864698e810edc740acf21ecc
Jonathan Rotter <hydrogen31415@gmail.com>
2020-04-01 03:19:52 +0200

    biome and fix to outpost

--- outpost.py
+++ outpost.py
@@ -1,12 +1,13 @@
 from tools import InterfaceSuperType, interface
 from points import HexPoint
+from biome import Biome
 
 class Outpost(InterfaceSuperType):
     '''
     Interface for Settlements and Cities
     '''
     
-    def dieRolled(self, d: int) -> None:
+    def giveResource(self, b: Biome) -> None:
         pass
 
     def getPos(self) -> HexPoint:
