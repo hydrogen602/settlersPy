72b04dcd8fd203b1f873d2c84b15270214923aa7
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2020-06-16 02:09:27 +0200

    cleanup

--- jsonComm.md
+++ jsonComm.md
@@ -1,7 +1,4 @@
 
 # Client to Server
 
-- Actions
-    - Purchase
-        - Any sort of purchase from the bank should include `{"action": "purchase"}` in it.  
-            To specify which purchase, include a `"name"` key whose value is the good being bought. 
+idk
--- oldDir/functions.py
+++ oldDir/functions.py
@@ -14,21 +14,3 @@ def requireNotNone(obj: object):
     if obj is None:
         raise TypeError(f'Object may not be None')
 
-# def enforceType(func):
-#     assert callable(func)
-#     assert hasattr(func, '__annotations__')
-
-#     if 'return' not in func.__annotations__:
-#         raise AttributeError(f'Function {func.__name__} is missing a return type annotation')
-
-#     def wrapper(*args, **kwargs):
-
-#         pass
-
-#     return wrapper
-    
-
-# def foo(x: int) -> None:
-#     pass
-
-# enforceType(foo)
