32779d6d3b5833aa69a0b77b17779a069441ece4
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2020-08-15 01:09:36 +0200

    stuff

--- gameServer/playerCode/trade.py
+++ gameServer/playerCode/trade.py
@@ -8,18 +8,18 @@ from .player import Player
 class Trade:
 
     def __init__(self, cost: Dict[Resource, int], goodsOffered: Dict[Resource, int]):
-        self.__cost: Dict[Resource, int] = cost
-        self.__goodsOffered: Dict[Resource, int] = goodsOffered
+        self._cost: Dict[Resource, int] = cost
+        self._goodsOffered: Dict[Resource, int] = goodsOffered
     
     def purchase(self, player: Player):
         '''
         throws `ActionError`.
         player is the one accepting the trade
         '''
-        player.requireResources(self.__cost) # raises ActionError
+        player.requireResources(self._cost) # raises ActionError
 
-        player.takeResources(self.__cost)
-        player.giveResources(self.__goodsOffered)
+        player.takeResources(self._cost)
+        player.giveResources(self._goodsOffered)
 
 
 class InterPlayerTrade(Trade):
@@ -32,11 +32,16 @@ class InterPlayerTrade(Trade):
 
         InterPlayerTrade are for only a single trade and so become
         invalid after a successful trade.
+
+        Throws ActionError if the Offeror does not have what he offers
         '''
+        offeror.requireResources(goodsOffered) # throws ActionError
+
         self.__offeror: Player = offeror
         self.__offerees: List[Player] = offerees
 
-        self.__closedDeal = False
+        self.__closedDeal = False # whether the deal has been closed
+        self.__invalid = False # whether the deal has been invalidated
         super().__init__(cost, goodsOffered)
     
     def __isValidOfferee(self, player: Player) -> bool:
@@ -48,10 +53,30 @@ class InterPlayerTrade(Trade):
         # player in self.__offerees checks if the player is in the list of accepted players
             
     def purchase(self, player: Player):
+        # errors here would go to player
         if not self.__isValidOfferee(player):
             raise ActionError(f"Player is not allowed to take this trade offer")
+        if self.__invalid:
+            raise ActionError(f"This trade offer is invalid due to {self.__offeror.name}")
+        if self.__closedDeal:
+            raise ActionError(f"This offer has already been accepted")
 
+        if not self.__offeror.hasResources(self._goodsOffered):
+            # offeror no longer has what he offered
+            self.__invalid = True
+            raise ActionError(f"{self.__offeror.name} doesn't have what he/she offered")
 
-        super().purchase(player)
+        player.requireResources(self._cost)
+
+        # both have what they wish to trade
+
+        player.takeResources(self._cost)
+        self.__offeror.takeResources(self._goodsOffered)
+
+        player.giveResource(self._goodsOffered)
+        self.__offeror.giveResources(self._cost)
+
+        self.__closedDeal = True
+
 
 
